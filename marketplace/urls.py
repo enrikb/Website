6c3040479b790211e19d6b3183afb6abcb539aa1
SchawnnDev <iperskill@gmail.com>
2019-10-03 00:53:10 +0200

    created layout + each page has a link

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home),
+	path('', views.home),
+    path('home', views.home, name='home'),
+	path('login', views.login, name='login'),
+	path('shop', views.shop, name='shop'),
+	path('cart', views.cart, name='cart'),
+	path('about', views.about, name='about'),
 ]
