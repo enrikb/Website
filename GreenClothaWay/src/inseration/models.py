5ae6f10741c9bdfcb6c79a34ea9f288b3685bbae
Nico Diefenbacher <Nico.Diefenbacher@dm.de>
2019-12-02 13:26:33 +0100

    edited layout.html

--- GreenClothaWay/src/inseration/models.py
+++ GreenClothaWay/src/inseration/models.py
@@ -1,107 +1,65 @@
-from django.conf import settings
-try:
-    from django.core.urlresolvers import reverse
-except ImportError:
-    from django.urls import reverse
-from django.db import models
-from django.db.models import signals
-from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
+from datetime import timezone, datetime
 
-AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class InserationManager(models.Manager):
 
-    def inbox_for(self, user):
-        """
-        Returns all messages that were received by the given user and are not
-        marked as deleted.
-        """
-        return self.filter(
-            recipient=user,
-            recipient_deleted_at__isnull=True,
+    def create_inseration(self, title, username, description, images, category, size):
+        if not title:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must have a username")
+        if not images:
+            raise ValueError("Users must have a password")
+        if not category:
+            raise ValueError("Users must have an title")
+        if not size:
+            raise ValueError("Users must have an first name")
+
+        inseration = self.model(
+            username=username,
+            title=title,
+            description=description,
+            images=images,
+            category=category,
+            size=size,
+            inserted_at=datetime.now,
         )
 
-    def outbox_for(self, user):
-        """
-        Returns all messages that were sent by the given user and are not
-        marked as deleted.
-        """
-        return self.filter(
-            sender=user,
-            sender_deleted_at__isnull=True,
-        )
-
-    def trash_for(self, user):
-        """
-        Returns all messages that were either received or sent by the given
-        user and are marked as deleted.
-        """
-        return self.filter(
-            recipient=user,
-            recipient_deleted_at__isnull=False,
-        ) | self.filter(
-            sender=user,
-            sender_deleted_at__isnull=False,
-        )
+        inseration.save(using=self._db)
+        return inseration
 
 
-@python_2_unicode_compatible
-class Inseration(models.Model):
-    """
-    A private message from user to user
-    """
-    subject = models.CharField(_("Subject"), max_length=140)
-    description = models.TextField(_("Description"))
-    seller = models.ForeignKey(AUTH_USER_MODEL, related_name='sent_messages', verbose_name=_("Sender"), on_delete=models.PROTECT)
-    buyer = models.ForeignKey(AUTH_USER_MODEL, related_name='received_messages', null=True, blank=True, verbose_name=_("Recipient"), on_delete=models.SET_NULL)
-    sent_at = models.DateTimeField(_("sent at"), null=True, blank=True)
-    read_at = models.DateTimeField(_("read at"), null=True, blank=True)
-    replied_at = models.DateTimeField(_("replied at"), null=True, blank=True)
-    sender_deleted_at = models.DateTimeField(_("Sender deleted at"), null=True, blank=True)
-    recipient_deleted_at = models.DateTimeField(_("Recipient deleted at"), null=True, blank=True)
 
-    objects = MessageManager()
+class Account(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    images = models.ImageField()
+    category = models.CharField()
+    size = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
 
-    def new(self):
-        """returns whether the recipient has read the message or not"""
-        if self.read_at is not None:
-            return False
-        return True
+    MALE_OR_FEMALE = (
+        ('MA', 'Male'),
+        ('FE', 'Female'),
+    )
 
-    def replied(self):
-        """returns whether the recipient has written a reply to this message"""
-        if self.replied_at is not None:
-            return True
-        return False
+    objects = InserationManager()
+    REQUIRED_FIELDS = (
+        'username', 'password',
+        'title', 'first_name', 'last_name',
+        'street', 'plz',
+        'city', 'country',
+        'housenumber',)
 
     def __str__(self):
-        return self.subject
+        return self.email
 
-    def get_absolute_url(self):
-        return reverse('messages_detail', args=[self.id])
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
 
-    def save(self, **kwargs):
-        if not self.id:
-            self.sent_at = timezone.now()
-        super(Message, self).save(**kwargs)
-
-    class Meta:
-        ordering = ['-sent_at']
-        verbose_name = _("Message")
-        verbose_name_plural = _("Messages")
-
-
-def inbox_count_for(user):
-    """
-    returns the number of unread messages for the given user but does not
-    mark them seen
-    """
-    return Message.objects.filter(recipient=user, read_at__isnull=True, recipient_deleted_at__isnull=True).count()
-
-# fallback for email notification if django-notification could not be found
-if "pinax.notifications" not in settings.INSTALLED_APPS and getattr(settings, 'DJANGO_MESSAGES_NOTIFY', True):
-    from django_messages.utils import new_message_email
-    signals.post_save.connect(new_message_email, sender=Message)
+    def has_module_perms(self, app_label):
+        return True
