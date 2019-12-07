b83ea97a27d8057cd961e15071be7a843973d015
Zeno Berkhan <zenoberkhan@rocketmail.com>
2019-12-07 16:03:31 +0100

    crazy changes

--- GreenClothaWay/src/inseration/models.py
+++ GreenClothaWay/src/inseration/models.py
@@ -33,19 +33,6 @@ class InserationManager(models.Manager):
         return inseration
 
 
-<<<<<<< HEAD
-class Inseration(models.Model):
-    inserter = models.ForeignKey(AUTH_USER_MODEL, related_name='inserted_object', verbose_name=_("Inserter"),
-                                 on_delete=models.CASCADE)
-    title = models.CharField(max_length=50,null=False, blank=False)
-    description = models.TextField(max_length=500,null=False, blank=False)
-    images = models.ImageField(upload_to='images/',max_length=50,null=False, blank=False)
-    subcategory = models.CharField(max_length=50,null=False, blank=False)
-    category = models.CharField(max_length=50,null=False, blank=False)
-    size = models.CharField(max_length=50, null=False, blank=False)
-=======
-<<<<<<< HEAD
-=======
 class Inseration(models.Model):
     inserter = models.ForeignKey(Account, on_delete=models.CASCADE)
     title = models.CharField(max_length=50, null=False, blank=False)
@@ -54,12 +41,9 @@ class Inseration(models.Model):
     subcategory = models.CharField(max_length=50, null=False, blank=False)
     category = models.CharField(max_length=50, null=False, blank=False, choices=CATEGORY_CHOICES)
     size = models.CharField(max_length=50, null=False, blank=False)
->>>>>>> a12e047... worked on inseration module
->>>>>>> 2b4b6af1a3d507fb716183ea02ad7d1150381a8c
 
     inserted_at = models.DateTimeField(_("inserted_at"), auto_now_add=True)
 
-<<<<<<< HEAD
     objects = InserationManager()
 
 
@@ -74,17 +58,3 @@ class Inseration(models.Model):
         ordering = ['-inserted_at']
         verbose_name = _("Insertion")
         verbose_name_plural = _("Insertions")
-<<<<<<< HEAD
-
-
-
-
-<<<<<<< HEAD
-=======
-    def has_module_perms(self, app_label):
-        return True
-=======
->>>>>>> a12e047... worked on inseration module
->>>>>>> 2b4b6af1a3d507fb716183ea02ad7d1150381a8c
-=======
->>>>>>> 2081c79... worked on inseration, works now
